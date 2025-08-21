8d0b0ec11b7e74ebe715c3affd6f6aba65639555
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-08-21 20:46:45 +0300

    add account details fetch

--- src/app/accounts/actions.ts
+++ src/app/accounts/actions.ts
@@ -7,6 +7,55 @@ import { cookies } from "next/headers";
 
 const baseUrl = process.env.API_BASE_URL!;
 
+export async function getAccountDetailsAction(
+  accountId: string
+): Promise<APISuccessResponse<Account> | APIErrorResponse> {
+  const cookieStore = await cookies();
+
+  const token = cookieStore.get("token");
+
+  if (!token) {
+    return {
+      success: false,
+      status: 401,
+      message: "Unauthorized.",
+    } satisfies APIErrorResponse;
+  }
+
+  try {
+    const res = await fetch(`${baseUrl}/api/accounts/${accountId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+
+    if (!res.ok) {
+      try {
+        const error: APIErrorResponse = await res.json();
+        return error;
+      } catch {
+        return {
+          success: false,
+          status: res.status,
+          message: "An unexpected error occurred.",
+        } satisfies APIErrorResponse;
+      }
+    }
+
+    const result: APISuccessResponse<Account> = await res.json();
+
+    return result;
+  } catch {
+    return {
+      success: false,
+      status: 503,
+      message: "An unexpected error occurred.",
+    } satisfies APIErrorResponse;
+  }
+}
+
 export async function getAccountsAction(): Promise<APISuccessResponse<Account[]> | APIErrorResponse> {
   const cookieStore = await cookies();
 
