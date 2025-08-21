904947486241ac7b675f7512eefeb38f616ae628
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-08-21 16:42:24 +0300

    add account deletion

--- src/app/accounts/(components)/account-item.tsx
+++ src/app/accounts/(components)/account-item.tsx
@@ -1,19 +1,50 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Account } from "@/types/account";
-import { Banknote, Calendar, Hash } from "lucide-react";
+import { Banknote, Calendar, Hash, Loader2, Trash } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
+import { deleteAccountAction } from "../actions";
 
 type AccountItemProps = {
   account: Account;
 };
 
 export function AccountItem({ account }: AccountItemProps) {
+  const [isPending, startTransition] = useTransition();
+
+  function handleDeleteAccount() {
+    startTransition(async () => {
+      const res = await deleteAccountAction(account.id);
+
+      if (res.success) {
+        toast.success(res.message, { position: "top-center", closeButton: true });
+      } else {
+        toast.error(res.message, { position: "top-center", closeButton: true });
+      }
+    });
+  }
+
   return (
-    <Card className="rounded-md py-4 hover:shadow-md">
+    <Card className="rounded-md hover:shadow-md">
       <CardContent className="flex flex-col space-y-2">
         <div className="flex items-center justify-between">
           <h2 className="text-lg font-semibold">Name: {account.name}</h2>
+          <div className="space-x-1">
+            {/* <Button size="icon" variant="ghost">
+              <ArrowLeftRight width={16} height={16} />
+            </Button>
+
+            <Button size="icon" variant="ghost">
+              <PenLine width={16} height={16} />
+            </Button> */}
+
+            <Button size="icon" variant="ghost" onClick={handleDeleteAccount}>
+              {isPending ? <Loader2 className="animate-spin" /> : <Trash width={16} height={16} />}
+            </Button>
+          </div>
         </div>
 
         <div className="flex items-center gap-2 text-sm">
--- src/app/accounts/actions.ts
+++ src/app/accounts/actions.ts
@@ -2,6 +2,7 @@
 
 import { Account } from "@/types/account";
 import { APIErrorResponse, APISuccessResponse } from "@/types/api-response";
+import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
 const baseUrl = process.env.API_BASE_URL!;
@@ -43,6 +44,55 @@ export async function getAccountsAction(): Promise<APISuccessResponse<Account[]>
     }
 
     const result: APISuccessResponse<Account[]> = await res.json();
+
+    return result;
+  } catch {
+    return {
+      success: false,
+      status: 503,
+      message: "An unexpected error occurred.",
+    } satisfies APIErrorResponse;
+  }
+}
+
+export async function deleteAccountAction(accountId: string): Promise<APISuccessResponse<null> | APIErrorResponse> {
+  const cookieStore = await cookies();
+
+  const token = cookieStore.get("token");
+
+  if (!token) {
+    return {
+      success: false,
+      status: 401,
+      message: "Unauthorized.",
+    } satisfies APIErrorResponse;
+  }
+
+  try {
+    const res = await fetch(`${baseUrl}/api/accounts/${accountId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+
+    if (!res.ok) {
+      try {
+        const error: APIErrorResponse = await res.json();
+        return error;
+      } catch {
+        return {
+          success: false,
+          status: res.status,
+          message: "An unexpected error occurred.",
+        } satisfies APIErrorResponse;
+      }
+    }
+
+    const result: APISuccessResponse<null> = await res.json();
+
+    revalidatePath("/accounts");
 
     return result;
   } catch {
